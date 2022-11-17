489dab762a8efb076b483ecc767c0e3e976aa4be
walshdev <alexwalsh.dev@gmail.com>
2022-11-18 00:24:18 +0300

    upd

--- src/Console/MakeModuleStoreCommand.php
+++ src/Console/MakeModuleStoreCommand.php
@@ -52,6 +52,6 @@ class MakeModuleStoreCommand extends GeneratorCommand
     {
         $module = Str::pluralStudly($this->argument('name'));
 
-        return $rootNamespace . "\Modules\\$module" . "Store";
+        return $rootNamespace . "\Modules\\$module\\" . $module . "Store.php";
     }
 }
